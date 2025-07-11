ed0da1c1c344f972b8d3a36bbacabbff0b7e74f8
mukherR12 <saocompilations@gmail.com>
2025-07-11 15:16:49 +0100

    scrapped turnjs - add speech + change font

--- js/script.js
+++ js/script.js
@@ -1,19 +1,5 @@
 $(".flipbook").turn();
 
-document.getElementById("demo").innerHTML = `
-<p>
-  this is an example on line 1 <br>
-  and i want to continue on line 2
-</p>
-`;
-document.getElementById("demo2").innerHTML = "Hello World!";
-
-document.getElementById("demo3").innerHTML = "bombasticlap!";
-
-document.getElementById("demo4").innerHTML = "bombasticlap2!";
-
-document.getElementById("bouton").innerHTML = $('#flipbook').turn('page'); 
-
 function TextToSpeech() {
     const pages = $('#flipbook').turn('view'); // Get currently visible pages
     let combinedText = '';
